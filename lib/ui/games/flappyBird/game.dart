4acee1ac2265f27d24b225bc760ef8dc2b6272af
strathdeek <kstrath818@gmail.com>
2023-04-26 10:07:59 +0200

    wip

---
+++ lib/ui/games/flappyBird/game.dart
@@ -0,0 +1,9 @@
+import 'package:flame/game.dart';
+
+class FlappyBirdGame extends FlameGame {
+  @override
+  void update(double dt) {
+    // TODO: implement update
+    super.update(dt);
+  }
+}
---
+++ lib/ui/screens/flappy_bird_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FlappyBirdScreen extends StatelessWidget {
+  const FlappyBirdScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
