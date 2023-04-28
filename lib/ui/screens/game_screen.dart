9dfa72b8df80b75bc58737d42c6aef613d456405
Daniel Wiesheu <daniel.wiesheu@jambit.com>
2023-04-28 14:42:49 +0200

    Fix wrong owner name in winning screen

--- lib/ui/screens/game_screen.dart
+++ lib/ui/screens/game_screen.dart
@@ -27,23 +27,34 @@ class _GameScreenState extends State<GameScreen> {
   void initState() {
     BlocProvider.of<PlaceBloc>(context).add(PlaceGameStarted(widget.place));
     super.initState();
-    _confettiController = ConfettiController(duration: const Duration(seconds: 10));
+    _confettiController =
+        ConfettiController(duration: const Duration(seconds: 10));
   }
 
   Path drawCoffeeBean(Size size) {
     var path = Path();
     path.lineTo(size.width * 0.79, size.height * 0.02);
-    path.cubicTo(size.width * 0.84, size.height * 0.07, size.width * 0.7, size.height * 0.27, size.width * 0.49, size.height * 0.49);
-    path.cubicTo(size.width * 0.27, size.height * 0.71, size.width * 0.06, size.height * 0.84, size.width * 0.02, size.height * 0.79);
-    path.cubicTo(-0.03, size.height * 0.75, 0, size.height * 0.43, size.width * 0.22, size.height * 0.22);
-    path.cubicTo(size.width * 0.43, size.height * 0.01, size.width * 0.75, -0.03, size.width * 0.79, size.height * 0.02);
-    path.cubicTo(size.width * 0.79, size.height * 0.02, size.width * 0.79, size.height * 0.02, size.width * 0.79, size.height * 0.02);
+    path.cubicTo(size.width * 0.84, size.height * 0.07, size.width * 0.7,
+        size.height * 0.27, size.width * 0.49, size.height * 0.49);
+    path.cubicTo(size.width * 0.27, size.height * 0.71, size.width * 0.06,
+        size.height * 0.84, size.width * 0.02, size.height * 0.79);
+    path.cubicTo(-0.03, size.height * 0.75, 0, size.height * 0.43,
+        size.width * 0.22, size.height * 0.22);
+    path.cubicTo(size.width * 0.43, size.height * 0.01, size.width * 0.75,
+        -0.03, size.width * 0.79, size.height * 0.02);
+    path.cubicTo(size.width * 0.79, size.height * 0.02, size.width * 0.79,
+        size.height * 0.02, size.width * 0.79, size.height * 0.02);
     path.lineTo(size.width / 5, size.height * 0.98);
-    path.cubicTo(size.width * 0.16, size.height * 0.94, size.width * 0.3, size.height * 0.73, size.width * 0.51, size.height * 0.51);
-    path.cubicTo(size.width * 0.73, size.height * 0.3, size.width * 0.94, size.height * 0.16, size.width * 0.98, size.height / 5);
-    path.cubicTo(size.width * 1.03, size.height / 4, size.width, size.height * 0.57, size.width * 0.78, size.height * 0.78);
-    path.cubicTo(size.width * 0.57, size.height, size.width / 4, size.height * 1.03, size.width / 5, size.height * 0.98);
-    path.cubicTo(size.width / 5, size.height * 0.98, size.width / 5, size.height * 0.98, size.width / 5, size.height * 0.98);
+    path.cubicTo(size.width * 0.16, size.height * 0.94, size.width * 0.3,
+        size.height * 0.73, size.width * 0.51, size.height * 0.51);
+    path.cubicTo(size.width * 0.73, size.height * 0.3, size.width * 0.94,
+        size.height * 0.16, size.width * 0.98, size.height / 5);
+    path.cubicTo(size.width * 1.03, size.height / 4, size.width,
+        size.height * 0.57, size.width * 0.78, size.height * 0.78);
+    path.cubicTo(size.width * 0.57, size.height, size.width / 4,
+        size.height * 1.03, size.width / 5, size.height * 0.98);
+    path.cubicTo(size.width / 5, size.height * 0.98, size.width / 5,
+        size.height * 0.98, size.width / 5, size.height * 0.98);
     path.lineTo(size.width / 5, size.height * 0.98);
     return path;
   }
@@ -88,37 +99,34 @@ class _GameScreenState extends State<GameScreen> {
                     mainAxisSize: MainAxisSize.min,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      Stack(
-                        alignment: Alignment.center,
-                        children: [
-                          Text(
-                            "Your score: ${state.score}",
-                            style: Theme.of(context).textTheme.headlineMedium,
+                      Stack(alignment: Alignment.center, children: [
+                        Text(
+                          "Your score: ${state.score}",
+                          style: Theme.of(context).textTheme.headlineMedium,
+                        ),
+                        Align(
+                          alignment: Alignment.topCenter,
+                          child: ConfettiWidget(
+                            confettiController: _confettiController,
+                            blastDirectionality: BlastDirectionality.explosive,
+                            shouldLoop: false,
+                            colors: [
+                              Colors.orange.shade200,
+                              Colors.white,
+                              Colors.orange.shade700,
+                              Colors.grey.shade800,
+                            ],
+                            // createParticlePath: drawCoffeeBean, // define a custom coffee-bean path. Path isn't correct, needs to be fixed.
                           ),
-                          Align(
-                            alignment: Alignment.topCenter,
-                            child: ConfettiWidget(
-                              confettiController: _confettiController,
-                              blastDirectionality: BlastDirectionality.explosive,
-                              shouldLoop: false,
-                              colors: [
-                                Colors.orange.shade200,
-                                Colors.white,
-                                Colors.orange.shade700,
-                                Colors.grey.shade800,
-                              ],
-                              // createParticlePath: drawCoffeeBean, // define a custom coffee-bean path. Path isn't correct, needs to be fixed.
-                            ),
-                          ),
-                        ]
-                      ),
+                        ),
+                      ]),
                       const SizedBox(
                         height: 40,
                       ),
                       Text(
                         state.points > 0
-                            ? "You beat $ownerName's high score and won ${state.points} jamBeans!"
-                            : "You weren't able to crack $ownerName's high score of ${state.place.minigame.score}.",
+                            ? "You beat the high score and won ${state.points} jamBeans!"
+                            : "You weren't able to crack the high score of ${state.place.minigame.score}.",
                         style: Theme.of(context).textTheme.headlineSmall,
                         textAlign: TextAlign.center,
                       ),
